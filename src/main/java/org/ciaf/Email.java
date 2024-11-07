6a26234a794c7e973bcadefdc603a7a62cad55be
Jonathan Cañola Salazar <jocasa90@hotmail.com>
2024-11-07 16:24:04 -0500

    Created class Mail

---
+++ src/main/java/org/ciaf/Email.java
@@ -0,0 +1,58 @@
+package org.ciaf;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Email {
+
+    public static void sendEmail(String addressee, String
+            affair, String
+            message, File archiveAttach){
+
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", "smtp.example.com");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication(){
+                return new PasswordAuthentication("pruebasjocasa@gmail.com", "jcs123");
+            }
+        });
+
+        try {
+            Message mail = new MimeMessage(session);
+            mail.setFrom(new InternetAddress("pruebasjocasa@gmail.com"));
+            mail.setRecipients(Message.RecipientType.TO, InternetAddress.parse(addressee));
+            mail.setSubject(affair);
+
+            MimeBodyPart bodymessage = new MimeBodyPart();
+            bodymessage.setText(message);
+
+            MimeBodyPart affair2 = new MimeBodyPart();
+            affair2.attachFile(archiveAttach);
+
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(bodymessage);
+            multipart.addBodyPart(affair2);
+
+            mail.setContent(multipart);
+
+            Transport.send(mail);
+            System.out.println("Mail sent successfully.");
+
+        } catch (AddressException e) {
+            throw new RuntimeException(e);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
